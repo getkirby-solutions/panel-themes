b56087a4b6ee10cd70e9152af63e540614ef70d7
heyallan <allanmoreno@live.com>
2016-11-03 02:51:08 -0600

    Refactor the whole thing

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 Panel theme CSS
 
+## Install
+
+```php
+// site/config/config.php
+c::set('panel.stylesheet', 'https://unpkg.com/kirby-panel-themes/dist/{{color}}.css');
+```
+
+## Azure
+
 ![screenshot](panel-theme-azure.png)
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
+var gulp         = require('gulp');
 var less         = require('gulp-less');
 var autoprefixer = require('less-plugin-autoprefix');
+var autoprefix   = new autoprefixer({browsers:['last 2 versions']});
 var minifyCSS    = require('gulp-minify-css');
+var rename       = require('gulp-rename');
+var pkgJson      = require('./package.json');
 
 gulp.task('styles', function() {
-  return gulp.src('src/less/panel.less')
+  return gulp.src('src/less/bundle.less')
     .pipe(less({plugins:[autoprefix]}))
     .pipe(minifyCSS())
-    .pipe(gulp.dest('dist/assets/css/'));
+    .pipe(rename('azure.min.css'))
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('default', ['styles']);
